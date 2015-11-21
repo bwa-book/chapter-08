19663c0a33acf7eac4de76f2ca9aed576a6a8e04
Maurice Kelly <osv-github@chatswood.org.uk>
2015-11-21 23:10:31 +0000

    Initialise the glance

--- GlanceCommander/GlanceCommander WatchKit Extension/GlanceController.swift
+++ GlanceCommander/GlanceCommander WatchKit Extension/GlanceController.swift
@@ -13,8 +13,8 @@ class GlanceController: WKInterfaceController {
     }
 
     override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+        activateCommands(stop: false, kontinue: false)
     }
 
     override func didDeactivate() {
@@ -22,4 +22,15 @@ class GlanceController: WKInterfaceController {
         super.didDeactivate()
     }
 
+    private func activateCommands(stop stop: Bool, kontinue: Bool) {
+        var commands = [String]()
+        commands.append(stop ? "GlanceCommandActive" : "GlanceCommandInactive")
+        commands.append(kontinue ? "GlanceCommandActive" : "GlanceCommandInactive")
+
+        commandTable.setRowTypes(commands)
+        let command1 = commandTable.rowControllerAtIndex(0) as! GlanceCommand
+        let command2 = commandTable.rowControllerAtIndex(1) as! GlanceCommand
+        stop ? command1.activateStop() : command1.deactivateStop()
+        kontinue ? command2.activateContinue() : command2.deactivateContinue()
+    }
 }
