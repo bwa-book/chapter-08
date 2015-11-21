555ba97283ba6b1580720e722406012a70ed44cf
Maurice Kelly <osv-github@chatswood.org.uk>
2015-11-21 23:12:52 +0000

    Update the controller to command the glance

--- GlanceCommander/GlanceCommander WatchKit Extension/GlanceController.swift
+++ GlanceCommander/GlanceCommander WatchKit Extension/GlanceController.swift
@@ -15,6 +15,7 @@ class GlanceController: WKInterfaceController {
     override func willActivate() {
         super.willActivate()
         activateCommands(stop: false, kontinue: false)
+        updateCommands()
     }
 
     override func didDeactivate() {
@@ -32,5 +33,16 @@ class GlanceController: WKInterfaceController {
         let command2 = commandTable.rowControllerAtIndex(1) as! GlanceCommand
         stop ? command1.activateStop() : command1.deactivateStop()
         kontinue ? command2.activateContinue() : command2.deactivateContinue()
+    }
+
+    private func updateCommands() {
+        let delayTime = dispatch_time(DISPATCH_TIME_NOW, Int64(1 * Double(NSEC_PER_SEC)))
+        dispatch_after(delayTime, dispatch_get_main_queue()) {
+            if (arc4random_uniform(2) != 0) {
+                self.activateCommands(stop: false, kontinue: true)
+            } else {
+                self.activateCommands(stop: true, kontinue: false)
+            }
+        }
     }
 }
